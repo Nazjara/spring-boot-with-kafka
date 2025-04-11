769cd2bbe60e0b5dccb2197a64c29773887a8110
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2025-04-11 15:36:41 +0300

    Init commit

---
+++ src/test/java/com/nazjara/SpringBootWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazjara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithKafkaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
